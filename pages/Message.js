d4ce525ae94718542a42735eae5ba985758ba9f5
Mecki-Messer <mecki.messer.gaming@web.de>
2021-06-15 18:53:42 +0200

    basic frame

--- pages/Message.js
+++ pages/Message.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Message( {message} ) {
     return (
         <div>
-            {message}
+            <h4>{message.name}</h4>
+            <p>{message.msg}</p>
+            <p>[DEBUG]MessageID: {message.id}</p>
         </div>
     )
 }
--- pages/MessageBoard.js
+++ pages/MessageBoard.js
@@ -1,29 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import Message from './Message'
-
+import { v4 as uuidv4 } from 'uuid'
 export function MessageList({ messages }) {
-
     return (
         messages.map(message => {
-            return <Message message={message}/>
+            return <Message key={message.id} message={message}/>
         })
         
     )
 }
 
 export default function MessageBoard() {
-    const [messages, setMessages] = useState(["dope", "lmao", "coco best kaichou"])
+    const [messages, setMessages] = useState([])
+    const messageNameRef = useRef()
+    const messageMsgRef = useRef()
+
+    function uploadMessage(e){
+        const messageName = messageNameRef.current.value
+        const messageMsg = messageMsgRef.current.value
+        if(messageName === "" || messageMsg === "") return
+        //console.log(messageName)
+        //console.log(messageMsg)
+        setMessages(prevMessages => {
+            return [...prevMessages, {id:uuidv4(), name:messageName, msg:messageMsg}]
+        })
+
+        messageNameRef.current.value = null
+        messageMsgRef.current.value = null
+    }
 
     return (
         <>
         <h2>Welcome to the Message Board</h2>
         <h3>Write New Message!</h3>
         <p>Display Name:</p>
-        <input type="text" />
+        <input ref={messageNameRef} type="text" />
         <p>Your Message:</p>
-        <textarea name="message" cols="40" rows="5"></textarea>
+        <textarea ref={messageMsgRef} name="message" cols="40" rows="5"></textarea>
         <br></br>
-        <button>Submit!</button>
+        <button onClick={uploadMessage}>Submit!</button>
         <br></br>
         <h3>Messages for Coco</h3>
         <br></br>
