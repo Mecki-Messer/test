ec1ae2dbff5a4ccdfd72cc4e0c0fa6bcc0a00b68
Mecki-Messer <mecki.messer.gaming@web.de>
2021-06-15 17:59:46 +0200

    more basic stuff

--- pages/MessageBoard.js
+++ pages/MessageBoard.js
@@ -1,10 +1,22 @@
 import React, { useState } from 'react'
+import Message from './Message'
+
+export function MessageList({ messages }) {
+
+    return (
+        messages.map(message => {
+            return <Message message={message}/>
+        })
+        
+    )
+}
 
 export default function MessageBoard() {
+    const [messages, setMessages] = useState(["dope", "lmao", "coco best kaichou"])
+
     return (
         <>
         <h2>Welcome to the Message Board</h2>
-        <br></br>
         <h3>Write New Message!</h3>
         <p>Display Name:</p>
         <input type="text" />
@@ -12,6 +24,10 @@ export default function MessageBoard() {
         <textarea name="message" cols="40" rows="5"></textarea>
         <br></br>
         <button>Submit!</button>
+        <br></br>
+        <h3>Messages for Coco</h3>
+        <br></br>
+        <MessageList messages={messages}/>
         </>
     )
 }
